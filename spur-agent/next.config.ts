6dea1daf2af4e632b87d4e161c0ab06c980ece3f
PRAVESH DHAKAL <pravdhakal110@gmail.com>
2025-12-29 23:20:24 +0530

    building to prod

--- spur-agent/next.config.ts
+++ spur-agent/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",       // 👈 REQUIRED for static sites
   reactCompiler: true,
+  images: {
+    unoptimized: true,   // 👈 REQUIRED for static hosting
+  },
 };
 
 export default nextConfig;
